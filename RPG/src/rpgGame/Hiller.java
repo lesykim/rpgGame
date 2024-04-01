5826bad75d52a3d0830211a1f403041c1415449b
lesykim <trchol01@naver.com>
2024-04-01 17:49:30 +0900

    Create Hiller

---
+++ RPG/src/rpgGame/Hiller.java
@@ -0,0 +1,15 @@
+package rpgGame;
+
+public class Hiller extends Unit implements Skill{
+
+	public Hiller(String name) {
+		super(name);
+	}
+
+	@Override
+	public void runSkill() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
