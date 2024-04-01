16bbe5956ce2388110a954c40a0fd2920dfb8dfe
lesykim <trchol01@naver.com>
2024-04-01 18:04:42 +0900

    update power to limit

--- RPG/src/rpgGame/Unit.java
+++ RPG/src/rpgGame/Unit.java
@@ -1,5 +1,16 @@
 package rpgGame;
 
+import java.util.Random;
+
 public class Unit {
+	Random ran = new Random();
 	
+	String name;
+	int hp;
+	int maxhp;
+	int limit;
+	
+	public Unit(String name) {
+		this.name = name;
+	}
 }
