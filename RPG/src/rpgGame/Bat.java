7e9ab57c191beb652f41cf1bd09476f024657be6
lesykim <trchol01@naver.com>
2024-04-01 18:02:49 +0900

    update extends Monster

--- RPG/src/rpgGame/Bat.java
+++ RPG/src/rpgGame/Bat.java
@@ -1,12 +1,12 @@
 package rpgGame;
 
-public class Bat extends Unit{
+public class Bat extends Monster{
 	
 	public Bat(String name) {
 		super("박쥐");
 		this.hp = ran.nextInt(200)+100;
 		this.maxhp = hp;
-		this.power = ran.nextInt(20)+10;
+		this.limit = ran.nextInt(20)+10;
 	}
 
 }
