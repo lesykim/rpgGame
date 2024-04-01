6e6236942cd2160c711e4e680f22b830b878c5d3
lesykim <trchol01@naver.com>
2024-04-01 18:04:23 +0900

    Update Orc extends Monster

--- RPG/src/rpgGame/Orc.java
+++ RPG/src/rpgGame/Orc.java
@@ -1,12 +1,12 @@
 package rpgGame;
 
-public class Orc extends Unit{
+public class Orc extends Monster{
 	
 	public Orc() {
 		super("오크");
 		this.hp = ran.nextInt(200)+100;
 		this.maxhp = hp;
-		this.power = ran.nextInt(20)+10;
+		this.limit = ran.nextInt(20)+10;
 	}
 
 }
