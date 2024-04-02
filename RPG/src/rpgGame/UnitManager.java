96d0cf2ee0239f79fa2bd0b2aa8ab00e8ff2d0ee
lesykim <trchol01@naver.com>
2024-04-02 10:36:27 +0900

    Create UnitManager

---
+++ RPG/src/rpgGame/UnitManager.java
@@ -0,0 +1,39 @@
+package rpgGame;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class UnitManager {
+	private Random ran = new Random();
+	
+	private String packageName = "rpgGame.";
+	private String[] monsterNames = {"Bat","Orc","Wolf"};
+	public ArrayList<Monster> monsters;
+	public ArrayList<Hero> heros;
+	
+	public void settingMonster(int size) {
+		for(int i = 0; i<size; i++) {
+			int n = ran.nextInt(3);
+			try {
+				Class<?> monsterClass = Class.forName(packageName+monsterNames[n]);
+				Object obj = monsterClass.getDeclaredConstructor().newInstance();
+				
+				if(obj instanceof Monster) {
+					Monster monster = (Monster) obj;
+					monster.hp = ran.nextInt(200)+100;
+					monster.maxhp = monster.hp;
+					monster.limit = ran.nextInt(20)+10;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void settingHero() {
+		heros.add(new Warrior());
+		heros.add(new Wizard());
+		heros.add(new Hiller());
+	}
+	
+}
