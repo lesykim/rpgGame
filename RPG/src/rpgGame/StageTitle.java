c475437221e2010111bb071bf53f7e3357c39e4e
lesykim <trchol01@naver.com>
2024-04-02 12:24:41 +0900

    Update update()

--- RPG/src/rpgGame/StageTitle.java
+++ RPG/src/rpgGame/StageTitle.java
@@ -1,17 +1,22 @@
 package rpgGame;
 
 public class StageTitle extends Stage{
-
+	
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
+		while(true) {
+			System.out.println("==== TEXT RPG ====");
+			System.out.println("[시작]을 입력하세요.");
+			String start = sc.next();
+			if(start.equals("시작")) {
+				break;
+			}
+		}
+		GameManager.nextStage = "LOBBY";
 		return false;
 	}
 
 	@Override
-	public void init() {
-		// TODO Auto-generated method stub
-		
-	}
+	public void init() {}
 
 }
