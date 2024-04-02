78d8c682e6be50e3c2a5040a1c91cf829b8d691c
lesykim <trchol01@naver.com>
2024-04-02 10:47:49 +0900

    update hp,maxhp,limit to setHp(),setMaxhp(),setLimit()

--- RPG/src/rpgGame/Warrior.java
+++ RPG/src/rpgGame/Warrior.java
@@ -4,14 +4,11 @@ public class Warrior extends Hero{
 
 	public Warrior() {
 		super("전사");
-		this.hp = 1000;
-		this.maxhp = hp;
-		this.limit = ran.nextInt(30)+20;
 	}
 
 	@Override
 	public void Skill() {
-		
+		System.out.println("");
 	}
 
 }
