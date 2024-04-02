644568078d736f0e59c58cece249c2d31234ced9
lesykim <trchol01@naver.com>
2024-04-02 10:51:25 +0900

    delete hp, maxhp, limit

--- RPG/src/rpgGame/Wolf.java
+++ RPG/src/rpgGame/Wolf.java
@@ -2,11 +2,8 @@ package rpgGame;
 
 public class Wolf extends Monster{
 
-	public Wolf(String name) {
+	public Wolf() {
 		super("늑대");
-		this.hp = ran.nextInt(200)+100;
-		this.maxhp = hp;
-		this.limit = ran.nextInt(20)+10;
 	}
 
 }
