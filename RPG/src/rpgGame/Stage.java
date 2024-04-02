cc24d72119526063003968cd86d53cea4db24595
lesykim <trchol01@naver.com>
2024-04-02 12:24:13 +0900

    Add public Scanner

--- RPG/src/rpgGame/Stage.java
+++ RPG/src/rpgGame/Stage.java
@@ -1,6 +1,9 @@
 package rpgGame;
 
+import java.util.Scanner;
+
 public abstract class Stage {
+	public Scanner sc = new Scanner(System.in);
 	public abstract boolean update();
 	public abstract void init();
 }
