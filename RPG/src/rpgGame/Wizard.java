49ec0ee304bf1d6c60651cf745d888daccf95cd8
lesykim <trchol01@naver.com>
2024-04-02 15:37:56 +0900

    Update Skill()

--- RPG/src/rpgGame/Wizard.java
+++ RPG/src/rpgGame/Wizard.java
@@ -4,15 +4,16 @@ public class Wizard extends Hero {
 
 	public Wizard() {
 		super("마법사");
-		
 		this.setHp(800);
 		this.setMaxhp(getHp());
-		this.setLimit(ran.nextInt(30)+20);
+		this.setLimit(30);
+		this.setDefaultPower(20);
 	}
 
 	@Override
-	public void Skill() {
-		
+	public void Skill(Hero hero) {
+		hero.setShieldOn(true);
+		System.out.printf("[마법사]스킬을 [%s]에게 사용했습니다!\n[%s]에게 쉴드가 생성되었습니다.\n",hero.getName(),hero.getName());
 	}
-
+	
 }
