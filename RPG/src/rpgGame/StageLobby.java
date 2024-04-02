6f8ed90dd5a3ead8ced7f0282dffa9f5f768a183
lesykim <trchol01@naver.com>
2024-04-02 15:39:12 +0900

    Delete inputNumber()

--- RPG/src/rpgGame/StageLobby.java
+++ RPG/src/rpgGame/StageLobby.java
@@ -1,24 +1,6 @@
 package rpgGame;
 
 public class StageLobby extends Stage{
-
-	private int inputNumber() {
-		while(true) {
-			System.out.print(": ");
-			String select =sc.next();
-			int number = -1;
-			try {
-				number = Integer.parseInt(select);				
-			} catch (Exception e) {
-				System.err.println("숫자를 입력하세요.");
-			}
-			if(number == 1||number == 2) {
-				return number;
-			}else {
-				System.err.println("잘못된 메뉴");
-			}
-		}
-	}
 	
 	@Override
 	public boolean update() {
