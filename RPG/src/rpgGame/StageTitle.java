5819bd2879200982af31fec80057bb4314bb09bc
lesykim <trchol01@naver.com>
2024-04-02 11:54:57 +0900

    Create StageTitle

---
+++ RPG/src/rpgGame/StageTitle.java
@@ -0,0 +1,17 @@
+package rpgGame;
+
+public class StageTitle extends Stage{
+
+	@Override
+	public boolean update() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
