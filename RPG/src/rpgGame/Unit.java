fbc11be54d7a19b16e683b74af699e44f07a4e53
lesykim <trchol01@naver.com>
2024-04-02 10:48:55 +0900

    update hp,maxhp,limit

--- RPG/src/rpgGame/Unit.java
+++ RPG/src/rpgGame/Unit.java
@@ -3,14 +3,48 @@ package rpgGame;
 import java.util.Random;
 
 public class Unit {
-	Random ran = new Random();
+	public Random ran = new Random();
 	
-	String name;
-	int hp;
-	int maxhp;
-	int limit;
+	private String name;
+	private int hp;
+	private int maxhp;
+	private int limit;
 	
 	public Unit(String name) {
 		this.name = name;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getMaxhp() {
+		return maxhp;
+	}
+
+	public void setMaxhp(int maxhp) {
+		this.maxhp = maxhp;
+	}
+
+	public int getLimit() {
+		return limit;
+	}
+
+	public void setLimit(int limit) {
+		this.limit = limit;
+	}
+	
+	
 }
