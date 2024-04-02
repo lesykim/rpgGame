dd01019bda0a1488401bc5e1ef6ff98ab1ffa133
lesykim <trchol01@naver.com>
2024-04-02 11:55:26 +0900

    Add GameManager, Update run()

--- RPG/src/rpgGame/GameManager.java
+++ RPG/src/rpgGame/GameManager.java
@@ -1,10 +1,32 @@
 package rpgGame;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameManager {
+	static String nextStage = "";
+	String curStage = "";
 	
+	Map<String, Stage> stageList = new HashMap<>();
 	
+	public GameManager() {
+		stageList.put("TITLE", new StageTitle());
+		stageList.put("BATTLE", new StageBattle());
+		stageList.put("LOBBY", new StageLobby());
+		
+		nextStage = "TITLE";
+	}
+	
+	private boolean changeStage() {
+		return true;
+	}
 	
 	public void run() {
-		
+		while(true) {
+			if(!changeStage()) {
+				break;
+			}
+		}
+		System.err.println("GAME OVER");
 	}
 }
