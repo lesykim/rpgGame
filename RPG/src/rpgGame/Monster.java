55a889ecb899b16ddb1921fce14478b37cd20fc0
lesykim <trchol01@naver.com>
2024-04-03 11:44:53 +0900

    Create attack()

--- RPG/src/rpgGame/Monster.java
+++ RPG/src/rpgGame/Monster.java
@@ -6,4 +6,24 @@ public class Monster extends Unit{
 		super(name);
 	}
 
+	@Override
+	void attack(Unit target) {
+		int attack = ran.nextInt(getLimit())+getDefaultPower();
+		// 쉴드 구현
+		Hero hero = (Hero) target;
+		if(hero.isShieldOn()) {
+			System.out.printf("[%s]에게 쉴드가 있습니다. \n[%s]는 데미지를 입히지 못합니다.\n",target.getName(),getName());
+			hero.setShieldOn(false);
+			return;
+		}
+		int targetHp = target.getHp()-attack;
+		if(targetHp <= 0) {
+			System.out.printf("[%s]가 [%s]를 처치했습니다.\n",getName(),target.getName());
+			targetHp = 0;
+		}else {
+			System.out.printf("[%s]가 [%s]에게 %d의 데미지를 입힙니다.\n",getName(),target.getName(),attack);
+		}
+		target.setHp(targetHp);
+	}
+
 }
