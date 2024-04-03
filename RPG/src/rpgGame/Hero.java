1bda30b2ea87bb57b83ca3e48a68563903245a69
lesykim <trchol01@naver.com>
2024-04-03 11:45:21 +0900

    Update attack()

--- RPG/src/rpgGame/Hero.java
+++ RPG/src/rpgGame/Hero.java
@@ -7,8 +7,18 @@ public abstract class Hero extends Unit{
 		super(name);
 	}
 	
+	@Override
 	public void attack(Unit target) {
 		int attack = ran.nextInt(getLimit())+getDefaultPower();
+		// 전사 공격력2배스킬 유무 확인
+		if(this.getName().equals("전사")) {
+			Warrior warrior = (Warrior) this;
+			if(warrior.isSkillOn()) {
+				attack *= 2;
+				warrior.setSkillOn(false);
+				System.out.println("[전사] 스킬 사용으로 공격력이 2배 상승합니다.");
+			}
+		}
 		int targetHp = target.getHp()-attack;
 		
 		if(targetHp <= 0) {
